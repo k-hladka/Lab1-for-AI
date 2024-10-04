7e17a16f99653e0fe3bd9fb7351688e8e16c8514
Kateryna Hladka <zipz211_gkv@student.ztu.edu.ua>
2024-10-04 16:54:04 +0300

    Added classification random points x1 and x2 for ADN operator. Form was changed: added radio-buttons for switch operations (or/and)

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             button1 = new Button();
             graphic = new System.Windows.Forms.DataVisualization.Charting.Chart();
             richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            radioButtonOR = new RadioButton();
+            radioButtonAND = new RadioButton();
+            groupBox1 = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)graphic).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(208, 539);
+            button1.Location = new Point(6, 96);
             button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
             button1.Size = new Size(236, 65);
@@ -49,18 +54,18 @@
             // 
             // graphic
             // 
-            chartArea1.Name = "ChartArea1";
-            graphic.ChartAreas.Add(chartArea1);
-            graphic.Location = new Point(12, 45);
+            chartArea2.Name = "ChartArea1";
+            graphic.ChartAreas.Add(chartArea2);
+            graphic.Location = new Point(12, 209);
             graphic.Margin = new Padding(3, 4, 3, 4);
             graphic.Name = "graphic";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series1.Color = Color.Black;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            graphic.Series.Add(series1);
-            graphic.Size = new Size(664, 415);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = Color.Black;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            graphic.Series.Add(series2);
+            graphic.Size = new Size(664, 460);
             graphic.TabIndex = 2;
             graphic.Text = "chart1";
             // 
@@ -72,18 +77,67 @@
             richTextBox1.TabIndex = 3;
             richTextBox1.Text = "";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.WindowText;
+            label1.Location = new Point(0, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Оберіть функцію";
+            label1.Click += label1_Click_1;
+            // 
+            // radioButtonOR
+            // 
+            radioButtonOR.AutoSize = true;
+            radioButtonOR.Checked = true;
+            radioButtonOR.Location = new Point(6, 26);
+            radioButtonOR.Name = "radioButtonOR";
+            radioButtonOR.Size = new Size(50, 24);
+            radioButtonOR.TabIndex = 7;
+            radioButtonOR.TabStop = true;
+            radioButtonOR.Text = "OR";
+            radioButtonOR.UseVisualStyleBackColor = true;
+            radioButtonOR.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButtonAND
+            // 
+            radioButtonAND.AutoSize = true;
+            radioButtonAND.Location = new Point(6, 56);
+            radioButtonAND.Name = "radioButtonAND";
+            radioButtonAND.Size = new Size(62, 24);
+            radioButtonAND.TabIndex = 8;
+            radioButtonAND.Text = "AND";
+            radioButtonAND.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.GradientInactiveCaption;
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(radioButtonAND);
+            groupBox1.Controls.Add(radioButtonOR);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(493, 186);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1185, 682);
+            Controls.Add(groupBox1);
             Controls.Add(richTextBox1);
             Controls.Add(graphic);
-            Controls.Add(button1);
             Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
-            Text = "Form1";
+            Text = "OR";
             ((System.ComponentModel.ISupportInitialize)graphic).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -91,5 +145,9 @@
         private Button button1;
         private System.Windows.Forms.DataVisualization.Charting.Chart graphic;
         private RichTextBox richTextBox1;
+        private Label label1;
+        private RadioButton radioButtonOR;
+        private RadioButton radioButtonAND;
+        private GroupBox groupBox1;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Metrics;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -9,8 +11,8 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // create two's measurement array where values are randoms
             double[,] arr = new double[500, 2];
-             this.graphic.Series[0].Points.Clear();
             for (int i = 0; i < arr.GetLength(0); i++)
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
@@ -18,20 +20,40 @@ namespace WinFormsApp1
                     arr[i, j] = value.NextDouble();
                 }
 
+            //clear our chart field (field where graphics are drawn)
             this.graphic.Series[0].Points.Clear();
 
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 this.richTextBox1.Text += "x1= " + arr[i, 0] +
                     " x2= " + arr[i, 1] + " "
-                    + Neuron(arr[i, 0], arr[i, 1]) + " \n";
+                    + (this.radioButtonOR.Checked ? NeuronOR(arr[i, 0], arr[i, 1]) :  NeuronAND(arr[i, 0], arr[i, 1]))
+                    + " \n";
 
                 this.graphic.Series[0].Points.AddXY(arr[i, 0], arr[i, 1]);
             }
         }
-        private string Neuron(double x1, double x2)
+        private string NeuronOR(double x1, double x2)
         {
             return (x1 + x2 - 0.5) > 0 ? "klass A" : "klass B";
+        }
+        private string NeuronAND(double x1, double x2)
+        {
+            return (x1 + x2 - 1.5) > 0 ? "klass A" : "klass B";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
